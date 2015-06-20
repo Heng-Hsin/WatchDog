bfe1e046281ee8063eafa16be64f599210638c7a
Heng-Hsin <heng.hsin.chang@gmail.com>
2015-06-21 00:14:00 +0800

    Starting a java watchdog program

---
+++ src/test/TestOutput.java
@@ -0,0 +1,9 @@
+package test;
+
+public class TestOutput {
+
+	public static void main(String[] args) {
+		System.out.println("This is output to standard output");
+		System.err.println("This is output to standard error");
+	}
+}
