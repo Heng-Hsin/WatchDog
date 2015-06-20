e2eea6a47a333f01d5869929f9b28b40c99fe9f7
Heng-Hsin <heng.hsin.chang@gmail.com>
2015-06-21 00:33:44 +0800

    Testing the eclipse github push

--- src/test/TestOutput.java
+++ src/test/TestOutput.java
@@ -3,7 +3,7 @@ package test;
 public class TestOutput {
 
 	public static void main(String[] args) {
-		System.out.println("This is output to standard output");
+		System.out.println("This is output to standard output Hi I'm ted");
 		System.err.println("This is output to standard error");
 	}
 }
