402e48c11623e9f7da075d7936e09ff280f5fc34
HengHsin <heng.hsin.chang@gmail.com>
2015-09-13 14:52:02 +0800

    Starting To play with the log system

---
+++ src/LogSystem/LogTest.java
@@ -0,0 +1,11 @@
+package LogSystem;
+
+public class LogTest {
+
+	public static void main(String[] args) {
+		System.out.println("This is output to standard output Hi I'm ted");
+		System.err.println("This is output to standard error");
+	
+}
+
+}
